aed2aecb1ec8b5198abfcedfcae3615365b7025e
SLain <sl163@ya.ru>
2020-06-14 16:08:56 +0300

    Добавляет рабочий слайдер: js скрипт для слайдера;

--- #src/js/slider.js
+++ #src/js/slider.js
@@ -1,21 +1,3 @@
-// JS-ФУНКЦИЯ ОПРЕДЕЛЕНИЯ ПОДДЕРЖКИ WEBP
-function testWebP(callback) {
-
-    var webP = new Image();
-    webP.onload = webP.onerror = function () {
-    callback(webP.height == 2);
-    };
-    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-    }
-    
-    testWebP(function (support) {
-    
-    if (support == true) {
-    document.querySelector('body').classList.add('webp');
-    }else{
-    document.querySelector('body').classList.add('no-webp');
-    }
-    }); // Подключение скрипта для проверки поддержки webp формата и выбора правильного формата в css;
 const allSlides = document.querySelectorAll('.slider__item');
 const leftBtn = document.querySelector('.slider__btn-left');
 const rightBtn = document.querySelector('.slider__btn-right');
@@ -106,5 +88,4 @@ function displayRightSlide(numOfSlide) {
         }
         else allSlides[i].classList.add('slider__item-opacity');
     }
-}  // Слайдер;
-
+}
