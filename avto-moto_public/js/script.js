d4121e203d2dab3bdaf939fbe6c7499eaa87cc93
SLain <sl163@ya.ru>
2020-06-15 18:00:30 +0300

    Добавляет в блок контакты интерактивную карту от google maps;

--- avto-moto_public/js/script.js
+++ avto-moto_public/js/script.js
@@ -206,3 +206,12 @@ function checkTarget(event) {
 }
  // Подключение модального окна с формой отзывов;
  // Реализация добавления отзывов;
+function initMap() {
+    // The location of Uluru
+    var uluru = {lat: 59.968368, lng: 30.317514};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.querySelector('.contact__map'), {zoom: 15, center: uluru});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
+  } // Скрипт для отображения google maps;
